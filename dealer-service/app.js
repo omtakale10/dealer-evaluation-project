54020e881b950a5ad1b59f1b84870a11719178a2
omtakale10 <omtakale01@gmail.com>
2026-04-09 13:31:47 +0530

    added cors

--- dealer-service/app.js
+++ dealer-service/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 const dealers = {
   "Laptop": [
     {name: "Dealer A", price: 50000},
